3be7a8733d3f793f9a61353daa05baf73adacc9b
f4loi <manuelalburquerquelopez@gmail.com>
2025-10-31 17:32:38 +0100

    make

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include "raylib.h"
 
 int main() {
-    InitWindow(800, 600, "TestEngine");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello from TestEngine!", 200, 300, 20, DARKGRAY);
-        EndDrawing();
-    }
-
-    CloseWindow();
+   
     return 0;
 }
